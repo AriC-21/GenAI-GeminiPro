7923795d3c483e1233ba763bade805cae91aa970
arindam <ariyoucandoit@gmail.com>
2024-05-14 23:01:13 +0530

    added two py file

---
+++ sql.py
@@ -0,0 +1,56 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import sqlite3
+
+import google.generativeai as genai
+## Configure GenAI key
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_gemini_response(question, prompt):
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content([prompt[0], question])
+    return response.text
+
+def read_sql_query(sql_query,db):
+    conn = sqlite3.connect(db)
+    cur = conn.cursor()
+    cur.execute(sql_query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    return rows
+
+prompt = [
+    """
+    You are an expert in converting English queries to SQL queries!
+    The SQL database has the name STUDENTS and has the following columns: NAME, CLASS, SECTION.
+    For example,\nExample 1 - How many entries of records are present?,
+    the SQL command will be something like this SELECT COUNT(*) FROM STUDENTS;
+    \n Example 2 - Tell me all the students studying in Gen AI class?,
+    the SQL command will be something like this SELECT * FROM STUDENTS WHERE CLASS='Gen AI';
+    also the SQL code should not have ``` in beginning or end and sql word in output.
+    """
+]
+
+st.set_page_config(page_title="SQL Query Generator")
+st.header("Gemini App to Retrieve SQL Data")
+
+question = st.text_input("Input: ",key='input')
+submit = st.button("Ask the Question")
+
+if submit:
+    response = get_gemini_response(question,prompt)
+    # st.write(response)
+    # if "sql" in response:
+    #     st.write("Executing the SQL Query")
+    #     read_sql_query(response,'students.db')
+    # else:
+    #     st.write("Please ask a valid SQL question")
+    st.subheader("The Response is: ")
+    rows = read_sql_query(response,'students.db')
+    for row in rows:
+        print(row)
+        st.header(row)
---
+++ sqlite.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+connection = sqlite3.connect('students.db')
+curser = connection.cursor()
+
+table_info = """
+Create table STUDENTS(NAME VARCHAR(25), CLASS VARCHAR(25),
+SECTION VARCHAR(25));
+"""
+
+curser.execute(table_info)
+
+curser.execute("INSERT INTO STUDENTS VALUES('Ari', 'Gen AI', 'A')")
+curser.execute("INSERT INTO STUDENTS VALUES('Sudipta', 'WebD', 'B')")
+curser.execute("INSERT INTO STUDENTS VALUES('Arnab', 'DevOps', 'C')")
+curser.execute("INSERT INTO STUDENTS VALUES('Raj', 'AI', 'A')")
+curser.execute("INSERT INTO STUDENTS VALUES('Rahul', 'ML', 'B')")
+curser.execute("INSERT INTO STUDENTS VALUES('Rohit', 'Gen AI', 'C')")
+
+print("The inserted records are:\n")
+data = curser.execute("SELECT * FROM STUDENTS")
+for row in data:
+    print(row)

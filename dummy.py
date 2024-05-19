d1091a680498946b7ef796268c3cd6310ce1b69d
Arindam Chatterjee <92972785+AriC-21@users.noreply.github.com>
2024-05-19 20:17:35 +0530

    Delete dummy.py

--- dummy.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect('students.db') 
-cur = con.cursor()
-
-rows = cur.execute("SELECT * FROM ELECTION WHERE CONSTITUENCY='Karimganj' AND YEAR='2014'")
-
-for row in rows:
-    print(row)
